504ae1c3a8dc4f61ed079b69f63de96c454eeff8
raghavaadi <aadithyaraghav@gmail.com>
2022-08-22 21:47:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import { Routes, Route, Link } from "react-router-dom";
 import Register from "./pages/Register";
 import "./App.css";
@@ -13,26 +12,12 @@ function App() {
   );
 }
 
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
-
 function About() {
   return (
     <>
       <main>
-        <h2>Who are we?</h2>
-        <p>That feels like an existential question, don't you think?</p>
+        <h2>Hello</h2>
+        <p>This is Raghav here</p>
       </main>
       <nav>
         <Link to="/">Home</Link>
